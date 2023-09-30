bbbca3b189fd692340bb3c1c38f23566d667a90e
Joice Maciel <joicemacielmartins@gmail.com>
2023-09-30 15:12:46 -0300

    finalizada página sobre mim

--- javascript/menu-mobile.js
+++ javascript/menu-mobile.js
@@ -1,5 +1,7 @@
 let mobileMenu = document.querySelector(".mobile-menu");
 let navList = document.querySelector(".menu-navegacao-lista");
+let minhaFoto = document.querySelector(".minha-foto");
+let linkRedeSocial = document.querySelectorAll(".link-rede-social");
 
 mobileMenu.addEventListener("click", () => navList.classList.toggle("active"));
 
